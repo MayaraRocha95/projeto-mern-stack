ab557614baf62113a7e064063e2420bb9687949c
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-07-05 21:09:09 -0300

    add Try/Catch

--- API-BREAKINGNEWS/src/middlewares/global.middlewares.js
+++ API-BREAKINGNEWS/src/middlewares/global.middlewares.js
@@ -2,28 +2,36 @@ const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
 const validId = (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid ID" });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid ID" });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
-
-  next();
 };
 
 const validUser = async (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  const user = await userService.findByIdService(id);
+    const user = await userService.findByIdService(id);
 
-  if (!user) {
-    return res.status(400).send({ message: "user not found" });
-  }
+    if (!user) {
+      return res.status(400).send({ message: "user not found" });
+    }
 
-    req.id =  id;
+    req.id = id;
     req.user = user;
 
-  next();
+    next();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 };
 
 module.exports = { validId, validUser };
